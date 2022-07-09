74f29ffa0d4f736dd7279c3931057adea83d50ba
tomsan96 <gradada0507@gmail.com>
2022-07-09 14:00:29 +0900

    first commit

---
+++ sharehoshi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sharehoshi
+//
+//  2022/07/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sharehoshi/sharehoshiApp.swift
@@ -0,0 +1,17 @@
+//
+//  sharehoshiApp.swift
+//  sharehoshi
+//
+//  2022/07/09.
+//
+
+import SwiftUI
+
+@main
+struct sharehoshiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
