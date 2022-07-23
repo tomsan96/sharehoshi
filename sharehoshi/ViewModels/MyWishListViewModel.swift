6b1df72665dbce9d4773d3dc72ba901053706c65
tomsan96 <gradada0507@gmail.com>
2022-07-23 18:21:33 +0900

    firestoreから取得した欲しいものリストを画面表示させる

--- sharehoshi/ViewModels/MyWishListViewModel.swift
+++ sharehoshi/ViewModels/MyWishListViewModel.swift
@@ -9,23 +9,22 @@ import FirebaseAuth
 import FirebaseFirestore
 import FirebaseFirestoreSwift
 
-class MyWishListViewModel {
+class MyWishListViewModel: ObservableObject {
     let database = Firestore.firestore()
+    @Published var myWishList: [WishProduct] = []
 
-    func getMyWishList(uid: String) async throws -> [WishProduct] {
+    func getMyWishList(uid: String) async throws {
         let usersReference = database.collection("users")
-        var wishList: [WishProduct] = []
         let querySnapshot = try await usersReference.document(uid).collection("wishList").getDocuments()
 
         if !querySnapshot.isEmpty {
             for document in querySnapshot.documents {
-                guard let wishProduct = try? Firestore.Decoder().decode(WishProduct.self, from: document.data()) else { return [] }
-                wishList.append(wishProduct)
-                print(wishList)
+                guard let wishProduct = try? Firestore.Decoder().decode(WishProduct.self, from: document.data()) else { return }
+                myWishList.append(wishProduct)
+                print(myWishList)
             }
         } else {
             print("Document does not exist")
         }
-        return wishList
     }
 }
--- sharehoshi/Views/MyWishListView.swift
+++ sharehoshi/Views/MyWishListView.swift
@@ -6,21 +6,32 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseAuth
+import GoogleSignIn
 
 struct MyWishListView: View {
-    let wishProducts = [
-        WishProduct(id: "1", name: "商品a", imageUrl: nil, webUrl: nil, amount: 1000, createdAt: Date()),
-        WishProduct(id: "2", name: "商品b", imageUrl: nil, webUrl: nil, amount: 500, createdAt: Date()),
-        WishProduct(id: "3", name: "商品c", imageUrl: nil, webUrl: nil, amount: 2300, createdAt: Date()),
-        WishProduct(id: "4", name: "商品d", imageUrl: nil, webUrl: nil, amount: 100, createdAt: Date()),
-        WishProduct(id: "5", name: "商品e", imageUrl: nil, webUrl: nil, amount: 1100, createdAt: Date()),
-        WishProduct(id: "6", name: "商品f", imageUrl: nil, webUrl: nil, amount: 1000000, createdAt: Date())
-    ]
+    @EnvironmentObject var authenticationViewModel: AuthenticationViewModel
+    let myWishListViewModel = MyWishListViewModel()
+    var uid: String? {
+        get {
+            guard let uid = authenticationViewModel.uid else { return nil }
+            return uid
+        }
+    }
 
     var body: some View {
-        List(wishProducts) { product in
+
+        List(myWishListViewModel.myWishList) { product in
             WishListRowView(wishProduct: product)
                 .frame(height: 88)
+        }
+        .task {
+            Task {
+                do {
+                    try await self.myWishListViewModel.getMyWishList(uid: "90sZjw9tG8WQesffk0OKIMkG5Lf2")
+                }
+            }
         }
     }
 }
