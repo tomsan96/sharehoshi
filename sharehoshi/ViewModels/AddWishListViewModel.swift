9797471bd0041116e872e8ae494cc7dc9e636451
tomsan96 <gradada0507@gmail.com>
2022-07-25 22:48:21 +0900

    欲しいもの追加のviewModel作成

---
+++ sharehoshi/ViewModels/AddWishListViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  AddWishProductViewModel.swift
+//  sharehoshi
+//
+//  2022/07/24.
+//
+
+import Foundation
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+class AddWishListViewModel {
+    let database = Firestore.firestore()
+    func addWishProduct(uid: String, product: WishProduct) throws {
+        let usersReference = database.collection("users")
+        _ = try usersReference.document(uid).collection("wishList").addDocument(from: product)
+    }
+}
