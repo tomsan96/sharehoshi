60a07bb36102d3711bfaf41683cf4f7e085643d3
tomsan96 <gradada0507@gmail.com>
2022-07-28 09:25:51 +0900

    モデルの修正
    WishProductモデルのwebURLとimageURLをString型で保持する

--- sharehoshi/Models/WishProduct.swift
+++ sharehoshi/Models/WishProduct.swift
@@ -12,12 +12,12 @@ import FirebaseFirestoreSwift
 struct WishProduct: Codable, Identifiable {
     let id: String
     let name: String?
-    let imageUrl: URL?
-    let webUrl: URL?
+    let imageUrl: String?
+    let webUrl: String?
     let amount: Int?
     let createdAt: Date
 
-    init(id: String, name: String?, imageUrl: URL?, webUrl: URL?, amount: Int?, createdAt: Date) {
+    init(id: String, name: String?, imageUrl: String?, webUrl: String?, amount: Int?, createdAt: Date) {
         self.id = id
         self.name = name
         self.imageUrl = imageUrl
@@ -30,18 +30,8 @@ struct WishProduct: Codable, Identifiable {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         id = try container.decode(String.self, forKey: .id)
         name = try container.decode(String?.self, forKey: .name)
-        let webUrlString = try container.decode(String.self, forKey: .webUrl)
-        if webUrlString.isEmpty {
-            webUrl = nil
-        } else {
-            webUrl = URL(string: webUrlString)
-        }
-        let imageUrlString = try container.decode(String.self, forKey: .imageUrl)
-        if imageUrlString.isEmpty {
-            imageUrl = nil
-        } else {
-            imageUrl = URL(string: imageUrlString)
-        }
+        webUrl = try container.decode(String?.self, forKey: .webUrl)
+        imageUrl = try container.decode(String?.self, forKey: .imageUrl)
         amount = try container.decode(Int?.self, forKey: .amount)
         let createdAtTimeStamp = try container.decode(Timestamp.self, forKey: .createdAt)
         createdAt = createdAtTimeStamp.dateValue()
